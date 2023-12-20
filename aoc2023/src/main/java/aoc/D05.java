9114ce7f4446562048425ae38cfb698a1b178a38
Izosel41 <guichards@netcourrier.com>
2023-12-21 00:30:28 +0100

    feat: day 05

---
+++ aoc2023/src/main/java/aoc/D05.java
@@ -0,0 +1,213 @@
+package aoc;
+
+import lombok.Data;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class D05 {
+    public Long one(List<String> puzzleTest) {
+        List<Rule> seedToSoilRules = new ArrayList<>();
+        List<Rule> soilToFertilizer = new ArrayList<>();
+        List<Rule> fertilizerToWater = new ArrayList<>();
+        List<Rule> waterToLight = new ArrayList<>();
+        List<Rule> lightToTemperature = new ArrayList<>();
+        List<Rule> temperatureToHumidity = new ArrayList<>();
+        List<Rule> humidityToLocation = new ArrayList<>();
+
+        List<Long> seeds = Arrays.stream(puzzleTest.getFirst().split(": ")[1].split(" "))
+                .map(String::trim)
+                .mapToLong(Long::parseLong)
+                .boxed().toList();
+
+        int i = 3;
+        while (!puzzleTest.get(i).isEmpty()) {
+            seedToSoilRules.add(buildRule(Arrays.stream(puzzleTest.get(i).split(" "))
+                    .map(String::trim)));
+            i++;
+        }
+
+        i += 2;
+        while (!puzzleTest.get(i).isEmpty()) {
+            soilToFertilizer.add(buildRule(Arrays.stream(puzzleTest.get(i).split(" "))
+                    .map(String::trim)));
+            i++;
+        }
+
+        i += 2;
+        while (!puzzleTest.get(i).isEmpty()) {
+            fertilizerToWater.add(buildRule(Arrays.stream(puzzleTest.get(i).split(" "))
+                    .map(String::trim)));
+            i++;
+        }
+
+        i += 2;
+        while (!puzzleTest.get(i).isEmpty()) {
+            waterToLight.add(buildRule(Arrays.stream(puzzleTest.get(i).split(" "))
+                    .map(String::trim)));
+            i++;
+        }
+
+        i += 2;
+        while (!puzzleTest.get(i).isEmpty()) {
+            lightToTemperature.add(buildRule(Arrays.stream(puzzleTest.get(i).split(" "))
+                    .map(String::trim)));
+            i++;
+        }
+
+        i += 2;
+        while (!puzzleTest.get(i).isEmpty()) {
+            temperatureToHumidity.add(buildRule(Arrays.stream(puzzleTest.get(i).split(" "))
+                    .map(String::trim)));
+            i++;
+        }
+
+        i += 2;
+        while (i < puzzleTest.size() && !puzzleTest.get(i).isEmpty()) {
+            humidityToLocation.add(buildRule(Arrays.stream(puzzleTest.get(i).split(" ")).map(String::trim)));
+            i++;
+        }
+
+        return seeds.stream()
+                .map(seed -> applyRules(seedToSoilRules, seed))
+                .map(soil -> applyRules(soilToFertilizer, soil))
+                .map(fertilizer -> applyRules(fertilizerToWater, fertilizer))
+                .map(water -> applyRules(waterToLight, water))
+                .map(light -> applyRules(lightToTemperature, light))
+                .map(temp -> applyRules(temperatureToHumidity, temp))
+                .map(hum -> applyRules(humidityToLocation, hum))
+                .min(Comparator.comparing(Long::valueOf))
+                .get();
+    }
+
+    private static Rule buildRule(Stream<String> puzzleTest) {
+        List<Long> ruleElements =
+                puzzleTest.mapToLong(Long::parseLong).boxed().toList();
+        Rule r = new Rule(ruleElements.get(0), ruleElements.get(1), ruleElements.get(2));
+        return r;
+    }
+
+    private Long applyRules(List<Rule> rules, Long seed) {
+        Optional<Rule> ruleToApply = rules.stream().filter(r -> r.isAppliable(seed)).findFirst();
+        if (ruleToApply.isEmpty())
+            return seed;
+        else
+            return ruleToApply.get().apply(seed);
+    }
+
+
+    public Long two(List<String> puzzleTest) {
+        List<Rule> seedToSoilRules = new ArrayList<>();
+        List<Rule> soilToFertilizer = new ArrayList<>();
+        List<Rule> fertilizerToWater = new ArrayList<>();
+        List<Rule> waterToLight = new ArrayList<>();
+        List<Rule> lightToTemperature = new ArrayList<>();
+        List<Rule> temperatureToHumidity = new ArrayList<>();
+        List<Rule> humidityToLocation = new ArrayList<>();
+
+        List<Long> seedRanges = Arrays.stream(puzzleTest.getFirst().split(": ")[1].split(" "))
+                .map(String::trim)
+                .mapToLong(Long::parseLong)
+                .boxed().toList();
+
+        int i = 3;
+        while (!puzzleTest.get(i).isEmpty()) {
+            seedToSoilRules.add(buildRule(Arrays.stream(puzzleTest.get(i).split(" "))
+                    .map(String::trim)));
+            i++;
+        }
+
+        i += 2;
+        while (!puzzleTest.get(i).isEmpty()) {
+            soilToFertilizer.add(buildRule(Arrays.stream(puzzleTest.get(i).split(" "))
+                    .map(String::trim)));
+            i++;
+        }
+
+        i += 2;
+        while (!puzzleTest.get(i).isEmpty()) {
+            fertilizerToWater.add(buildRule(Arrays.stream(puzzleTest.get(i).split(" "))
+                    .map(String::trim)));
+            i++;
+        }
+
+        i += 2;
+        while (!puzzleTest.get(i).isEmpty()) {
+            waterToLight.add(buildRule(Arrays.stream(puzzleTest.get(i).split(" "))
+                    .map(String::trim)));
+            i++;
+        }
+
+        i += 2;
+        while (!puzzleTest.get(i).isEmpty()) {
+            lightToTemperature.add(buildRule(Arrays.stream(puzzleTest.get(i).split(" "))
+                    .map(String::trim)));
+            i++;
+        }
+
+        i += 2;
+        while (!puzzleTest.get(i).isEmpty()) {
+            temperatureToHumidity.add(buildRule(Arrays.stream(puzzleTest.get(i).split(" "))
+                    .map(String::trim)));
+            i++;
+        }
+
+        i += 2;
+        while (i < puzzleTest.size() && !puzzleTest.get(i).isEmpty()) {
+            humidityToLocation.add(buildRule(Arrays.stream(puzzleTest.get(i).split(" ")).map(String::trim)));
+            i++;
+        }
+
+        List<Long> seeds = new ArrayList<>();
+
+        for (int j = 0; j < seedRanges.size(); j = j + 2) {
+            Long start = seedRanges.get(j);
+            Long range = seedRanges.get(j + 1);
+            for (int k = 0; k < range; k++) {
+                seeds.add(start + k);
+            }
+        }
+
+        return seeds.stream()
+                .map(seed -> applyRules(seedToSoilRules, seed))
+                .map(soil -> applyRules(soilToFertilizer, soil))
+                .map(fertilizer -> applyRules(fertilizerToWater, fertilizer))
+                .map(water -> applyRules(waterToLight, water))
+                .map(light -> applyRules(lightToTemperature, light))
+                .map(temp -> applyRules(temperatureToHumidity, temp))
+                .map(hum -> applyRules(humidityToLocation, hum))
+                .min(Comparator.comparing(Long::valueOf))
+                .get();
+    }
+
+    public Long seedToSoil(String ruleDef, Long seed) {
+        Rule rule = buildRule(Arrays.stream(ruleDef.split(" ")));
+        return rule.apply(seed);
+    }
+}
+
+@Data
+class Rule {
+    Long destinationStart;
+    Long sourceStart;
+    Long range;
+
+    public Rule(Long destinationStart, Long sourceStart, Long range) {
+        this.destinationStart = destinationStart;
+        this.sourceStart = sourceStart;
+        this.range = range;
+
+    }
+
+    Long apply(Long seed) {
+        if (isAppliable(seed))
+            return destinationStart + (seed - sourceStart);
+        else
+            return seed;
+    }
+
+    boolean isAppliable(Long seed) {
+        return sourceStart <= seed
+                && seed < sourceStart + range;
+    }
+}
